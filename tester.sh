8802658f71cd423941c7f45af509ae009cd248fe
Canh Do Minh <minhcanh99@gmail.com>
2024-10-17 11:40:12 +0200

    Refinding specification

--- tester.sh
+++ tester.sh
@@ -4,7 +4,7 @@ echo "~~~~~~~~~~ TESTING ~~~~~~~~~~"
 DIR="test"
 BASEDIR=$PWD
 ls -d */ | sort -V | while read -r PROB; do
-    cd $BASEDIR/$PROB && (ls | grep ".maude" | sort -V) | while read -r FILE; do
+    cd $BASEDIR/$PROB && (ls | grep -E "(tas|qlock).maude" | sort -V) | while read -r FILE; do
         NAME=`echo $FILE | cut -d '.' -f1`
         echo "--> $NAME"
         maude < $FILE -no-banner | sed -r '/^(rewrites:|Solution|states:)/d' > "$DIR/$NAME.out"
